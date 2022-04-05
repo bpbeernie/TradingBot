9119982eff19af25495b9b1dbf2ec96290efc728
Benny Poon <bpbeernie@gmail.com>
2022-04-04 22:49:56 -0700

    added more test cases

--- Test/test_amd_bot.py
+++ Test/test_amd_bot.py
@@ -4,46 +4,65 @@ from unittest.mock import patch, Mock
 import sys
 sys.modules['gb'] = Mock()
 from AMDStrategy.AggressiveAMDOpenBot import AggressiveAMDBot
-from pprint import pprint
 from random import seed
 from random import uniform
+from random import randint
+from Test.TestData import *
+
 
 class TestAMDBot(unittest.TestCase):
-    
-    def test_on_realtime_update(self):
+    @classmethod
+    def setUpClass(cls):
         requestID = 1
         ib = Mock()
         ib.reqRealTimeBars = Mock(return_value = 1)
         
-        bot = AggressiveAMDBot(ib, "AAPL")
-        self.assertEqual(bot.reqId, requestID, "Request ID Set Properly")
+        cls.bot = AggressiveAMDBot(ib, "AMD")
+
+        cls.bot.setup()
         
-        bot.setup()
+        testBars = []
         
-        
-        testBars = self.generateBars(177.88, 178.02, 177.63, 177.64)
-        testBars2 = self.generateBars(177.63, 177.75, 177.45, 177.74)
+        for bar in sadAndHappyPath:
+            testBars.extend(cls.generateBars(*bar))
         
         for bar in testBars:
-            bot.on_realtime_update(requestID, None, bar[0], bar[1], bar[2], bar[3], None, None, None)
+            cls.bot.on_realtime_update(requestID, None, bar[0], bar[1], bar[2], bar[3], None, None, None)
+            
+    def test_on_openBar(self):
+        self.assertEqual(self.bot.openBar.low, 110.43, "Low setup properly")
+        self.assertEqual(self.bot.openBar.high, 110.96, "High setup properly")
         
-        for bar in testBars2:
-            bot.on_realtime_update(requestID, None, bar[0], bar[1], bar[2], bar[3], None, None, None)
+    def test_entry_triggers_quantity(self):
+        self.assertEqual(self.bot.quantity, 34, "Test Quantity")
         
-        bot.on_realtime_update(requestID, None, bar[0], bar[1], bar[2], bar[3], None, None, None)
+    def test_entry_triggers_profitTargets(self):
+        self.assertEqual(round(self.bot.profitTargetForLong, 2), 113.29, "Test Profit Target Long")
+        self.assertEqual(round(self.bot.profitTargetForShort, 2), 108.1, "Test Profit Target Short")
         
-        pprint(bot.openBar)
-        
-        
+    def test_entry_triggers_StopLosses(self):
+        self.assertEqual(round(self.bot.stopLossForLong, 2), 110.32, "Test Stop Loss Long")
+        self.assertEqual(round(self.bot.stopLossForShort, 2), 111.07, "Test Stop Loss Short")
+
+    @classmethod
     def generateBars(self, _open, high, low, _close):
         seed(1)
         newStart = _open
         returnList = []
         
+        highIndex = randint(1, 12)
+        highLow = randint(1, 12)
+        
         for i in range(12):
             newHigh = uniform(newStart, high)
             newClose = uniform(newStart, newHigh)
             newLow = uniform(low, newClose)
+            
+            if i == highIndex:
+                newHigh = high
+                
+            if i == highLow:
+                newLow = low
             
             if i == 11:
                 returnList.append((newStart, newHigh, newLow, _close))
