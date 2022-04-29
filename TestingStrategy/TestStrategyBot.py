37cdb4e43710e1f8a5a82f4d00da956e5901cbb1
Benny Poon <bpbeernie@gmail.com>
2022-04-28 20:40:04 -0700

    more fixes

--- TestingStrategy/TestStrategyBot.py
+++ TestingStrategy/TestStrategyBot.py
@@ -1,16 +1,12 @@
-#Imports
 from ibapi.contract import Contract
-from Helpers import Bars as bars, Orders as orders
+from Helpers import Bars as bars
 from Globals import Globals as gb
 import logging
 import os
 import datetime
-import pytz
-import math
-from AMDStrategy import AMDExecutionTracker as tracker
 import threading
-import sys
 import csv
+from TestingStrategy import Constants as const
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -25,25 +21,23 @@ logger.addHandler(file_handler)
 #Bot Logic
 class TestBot:
     lock = threading.Lock()
+    analysisLock = threading.Lock()
+    
     
     def __init__(self, ib, symbol):
         self.ib = ib
         self.symbol = symbol
         self.reqIdList = []
-        self.startingBars = []
+        self.processedReqIdList = []
+        
         self.barsize = 1
         self.openBar = None
-        self.targetPrice = 0
-        self.executionTracker = None
-        self.quantity = 0
         self.entryLimitforShort = 0.0
         self.profitTargetForShort = 0.0
         self.stopLossForShort = 0.0
         self.entryLimitforLong = 0.0
         self.profitTargetForLong = 0.0
         self.stopLossForLong = 0.0
-        self.timingCounter = 0
-        self.done = False
         self.data = {}
         
         self.dateCount = 0
@@ -56,38 +50,43 @@ class TestBot:
         self.finalResults = {}
 
     def setup(self):
-        self.executionTracker = tracker.AMDExecutionTracker()
-        
-        #Create our IB Contract Object
         self.contract = Contract()
         self.contract.symbol = self.symbol.upper()
         self.contract.secType = "STK"
         self.contract.exchange = "SMART"
         self.contract.currency = "USD"
         self.contract.primaryExchange = "ARCA"
-
-        # Request Historical Data
-        start_date = datetime.datetime(2022, 3, 11)
-        end_date = datetime.datetime.now() + datetime.timedelta(days=1)
         
-        date_range = self.workdays(start_date, end_date)
-        self.dateCount = len(date_range)
+        for dateRange in const.DATE_RANGE:
+            TestBot.lock.acquire()
+            self.reqIdList = []
+            self.processedReqIdList = []
+            self.data = {}
+            self.finalResults = {}
+            
+            self.folderName = dateRange[0]
+            
+            start_date = dateRange[1] + datetime.timedelta(days=1)
+            end_date = dateRange[2]
         
-        TestBot.lock.acquire()
-        print("Starting " + self.symbol)
-        for single_date in date_range:
-            queryTime = single_date.strftime("%Y%m%d %H:%M:%S")
-            reqId = gb.Globals.getInstance().getOrderId()
-            self.ib.reqHistoricalData(reqId, self.contract,queryTime,"1 D",str(self.barsize)+ " min","TRADES",1,1,False,[])
-            self.reqIdList.append(reqId)
+            date_range = self.workdays(start_date, end_date)
+            self.dateCount = len(date_range)
+            
+            print("Starting " + self.symbol)
+            for single_date in date_range:
+                queryTime = single_date.strftime("%Y%m%d %H:%M:%S")
+                reqId = gb.Globals.getInstance().getOrderId()
+                self.ib.reqHistoricalData(reqId, self.contract,queryTime,"1 D",str(self.barsize)+ " min","TRADES",1,1,False,[])
+                self.reqIdList.append(reqId)
 
 
     def workdays(self, d, end, excluded=(6, 7)):
         days = []
         while d.date() <= end.date():
-            if d.isoweekday() not in excluded:
+            if d.isoweekday() not in excluded and d not in const.HOLIDAYS:
                 days.append(d)
             d += datetime.timedelta(days=1)
+            
         return days
 
     def on_bar_update(self, reqId, bar, realtime):
@@ -105,75 +104,94 @@ class TestBot:
         
         self.data.setdefault(dateString, []).append(newBar) 
         
+    def proccessDate(self, dateToProcess):
+        result = 0
+        longEntryDone = False
+        shortEntryDone = False
+        longEntryRunning = False
+        shortEntryRunning = False
+        openBar = None
+        data = self.data[dateToProcess]
+        
+        for newBar in data:                        
+            if not openBar:
+                openBar = newBar
+
+                diff = openBar.high - openBar.low
+                adjustedHigh = openBar.high + diff * 0.2
+                adjustedLow = openBar.low - diff * 0.2
+                adjustedDiff = adjustedHigh - adjustedLow
+                
+                entryLimitforLong = adjustedHigh
+                profitTargetForLong = adjustedHigh + adjustedDiff * 3
+                stopLossForLong = adjustedLow
+                
+                entryLimitforShort = adjustedLow
+                profitTargetForShort = adjustedLow - adjustedDiff * 3
+                stopLossForShort = adjustedHigh
+
+            else:
+                if longEntryDone and shortEntryDone:
+                    return result
+                
+                if longEntryRunning and not longEntryDone:
+                    if newBar.high > profitTargetForLong:
+                        result += 3
+                        return result
+                    if newBar.low < stopLossForLong:
+                        shortEntryRunning = True
+                        longEntryDone = True
+                        result -= 1
+                        
+                if shortEntryRunning and not shortEntryDone:
+                    if newBar.low < profitTargetForShort:
+                        result += 3
+                        return result
+                    
+                    if newBar.high > stopLossForShort:
+                        longEntryRunning = True
+                        shortEntryDone = True
+                        result -= 1
+                
+                if not longEntryRunning and not shortEntryRunning:
+                    if newBar.high > entryLimitforLong:
+                        longEntryRunning = True
+                    
+                    if newBar.low < entryLimitforShort:
+                        shortEntryRunning = True
+                        
+        if shortEntryRunning and not shortEntryDone:
+            remaining =  (entryLimitforShort - data[-1].close) / (entryLimitforShort - profitTargetForShort)
+            result += remaining * 3
+            
+        if longEntryRunning and not longEntryDone:
+            remaining = (data[-1].close - entryLimitforLong) /  (profitTargetForLong - entryLimitforLong)
+            result += remaining * 3
+            
+        return result
+        
+        
     def historicalDataEnd(self,reqId):
         if reqId not in self.reqIdList:
             return
         
-        for date in self.data.keys():
-            result = 0
-            done = False
-            
-            for newBar in self.data[date]:
-                    
-                if not self.openBar:
-                    self.initialize(newBar)
-                else:
-                    if self.longEntryDone and self.shortEntryDone:
-                        done = True
-                        break
-                    
-                    if self.longEntryRunning and not self.longEntryDone:
-                        if newBar.high > self.profitTargetForLong:
-                            #print("Long profit target hit")
-                            result += 3
-                            done = True
-                            break
-                        if newBar.low < self.stopLossForLong:
-                            #print("Long stop loss hit")
-                            self.shortEntryRunning = True
-                            self.longEntryDone = True
-                            #print("Starting Short entry")
-                            result -= 1
-                            
-                    if self.shortEntryRunning and not self.shortEntryDone:
-                        if newBar.low < self.profitTargetForShort:
-                            #print("Short profit target hit")
-                            result += 3
-                            done = True
-                            break
-                        if newBar.high > self.stopLossForShort:
-                            #print("Short stop loss hit")
-                            self.longEntryRunning = True
-                            self.shortEntryDone = True
-                            #print("Starting long Entry")
-                            result -= 1
-                    
-                    if not self.longEntryRunning and not self.shortEntryRunning:
-                        if newBar.high > self.entryLimitforLong:
-                            #print("Entered Long at: " + str(self.entryLimitforLong))
-                            self.longEntryRunning = True
-                        
-                        if newBar.low < self.entryLimitforShort:
-                            #print("Entered Short at: " + str(self.entryLimitforShort))
-                            self.shortEntryRunning = True
-            if not done:
-                if self.shortEntryRunning and not self.shortEntryDone:
-                    remaining =  (self.entryLimitforShort - self.data[date][-1].close) / (self.entryLimitforShort - self.profitTargetForShort)
-                    result += remaining * 3
-                    
-                if self.longEntryRunning and not self.longEntryDone:
-                    remaining = (self.data[date][-1].close - self.entryLimitforLong) /  (self.profitTargetForLong - self.entryLimitforLong)
-                    result += remaining * 3
-            
-            self.finalResults.setdefault(datetime.datetime.strptime(date, '%Y-%m-%d'), result) 
-            self.reset()
-        if len(self.finalResults) == self.dateCount:
+        self.processedReqIdList.append(reqId)
+        
+        if len(self.reqIdList) == len(self.processedReqIdList):
+            for date in self.data.keys():
+                result = self.proccessDate(date)
+                self.finalResults.setdefault(datetime.datetime.strptime(date, '%Y-%m-%d'), result) 
+
             self.printFinalResults()
             print("Ending " + self.symbol)
             TestBot.lock.release()
         
     def printFinalResults(self):
-        with open('C:/Users/bpbee/Desktop/trading/BackTesting/' + self.symbol + ".csv", 'w', encoding='UTF8', newline='') as f:
+        folder = const.OUTPUT_PATH + f'{self.folderName}/'
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+            
+        with open(folder + self.symbol + ".csv", 'w', encoding='UTF8', newline='') as f:
             writer = csv.writer(f)
             writer.writerow([None, "Win", "Lose"])
         
@@ -186,29 +204,6 @@ class TestBot:
                 else:
                     writer.writerow([date, abs(value), None])
         
-        
-    def reset(self):
-        self.longEntryRunning = False
-        self.longEntryDone = False
-        self.shortEntryRunning = False
-        self.shortEntryDone = False
-        self.openBar = None
-        
-    def initialize(self, bar):
-        self.openBar = bar
-
-        diff = self.openBar.high - self.openBar.low
-        adjustedHigh = self.openBar.high + diff * 0.2
-        adjustedLow = self.openBar.low - diff * 0.2
-        adjustedDiff = adjustedHigh - adjustedLow
-        
-        self.entryLimitforLong = adjustedHigh
-        self.profitTargetForLong = adjustedHigh + adjustedDiff * 3
-        self.stopLossForLong = adjustedLow
-        
-        self.entryLimitforShort = adjustedLow
-        self.profitTargetForShort = adjustedLow - adjustedDiff * 3
-        self.stopLossForShort = adjustedHigh
 
     def updateStatus(self, orderID, status):
         pass
