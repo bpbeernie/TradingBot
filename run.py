741a86f66a73aac77ca6d9cec86702958251e93d
Benny Poon <bpbeernie@gmail.com>
2022-06-08 16:55:06 -0700

    move to v3

--- run.py
+++ run.py
@@ -50,10 +50,10 @@ def run():
     #botList.append(AggressiveAMDOpenBot.AggressiveAMDBot(ib, "TWTR")) 
     #botList.append(AggressiveAMDOpenBot.AggressiveAMDBot(ib, "AAPL")) 
     
-    botList.append(AggressiveAMDOpenBotV2.AggressiveAMDBotV2(ib, "AAPL"))
+    botList.append(AggressiveAMDOpenBotV3.AggressiveAMDBotV3(ib, "AAPL"))
     #botList.append(AggressiveAMDOpenBotV2.AggressiveAMDBotV2(ib, "FB")) 
-    botList.append(AggressiveAMDOpenBotV2.AggressiveAMDBotV2(ib, "TWTR")) 
-    botList.append(AggressiveAMDOpenBotV2.AggressiveAMDBotV2(ib, "GM")) 
+    botList.append(AggressiveAMDOpenBotV3.AggressiveAMDBotV3(ib, "TWTR")) 
+    botList.append(AggressiveAMDOpenBotV3.AggressiveAMDBotV3(ib, "GM")) 
     
     botList.append(AggressiveAMDOpenBotV3.AggressiveAMDBotV3(ib, "FB")) 
     #testIB = IBTestApi(ib)
