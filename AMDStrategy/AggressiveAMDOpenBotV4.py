c59dbfe216bc1356777bc3fb68dbdcca37eb4a61
Benny Poon <bpbeernie@gmail.com>
2023-01-24 05:41:49 -0800

    emergency fix

--- AMDStrategy/AggressiveAMDOpenBotV4.py
+++ AMDStrategy/AggressiveAMDOpenBotV4.py
@@ -107,7 +107,7 @@ class AggressiveAMDBotV4(OpenBotBase.OpenBotBase):
 
     def cancel_entry_order(self, orderID):
         logger.info(self.symbol + " canceling")
-        self.ib.cancelOrder(orderID)
+        self.ib.cancelOrder(orderID, "")
         self.done = True
        
     def processBar(self, time, open_, high, low, close, volume):
